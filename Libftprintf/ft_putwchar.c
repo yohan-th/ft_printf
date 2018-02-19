ecf2b9a3a39d17ccc926c853cdd304d332c789ab
Yohan Thollet <ythollet@z1r11p4.le-101.fr>
2018-02-19 16:38:43 +0100

    ft_printf

--- Libftprintf/ft_putwchar.c
+++ Libftprintf/ft_putwchar.c
@@ -19,22 +19,22 @@
 
 int		ft_putwchar(wchar_t c)
 {
-	if (c <= 0x7F)
-		return (ft_putchar(c));
-	else if (c <= 0x7FF)
+	if (c <= 255)
+		return (write(1, &c, 1));
+	else if (c <= 2047)
 	{
 		ft_putchar((c >> 6) | 0xC0);
 		ft_putchar((c & 0x3F) | 0x80);
 		return (2);
 	}
-	else if (c <= 0xFFFF)
+	else if (c <= 65535)
 	{
 		ft_putchar((c >> 12) | 0xE0);
 		ft_putchar(((c >> 6) & 0x3F) | 0x80);
 		ft_putchar((c & 0x3F) | 0x80);
 		return (3);
 	}
-	else if (c <= 0x10FFFF)
+	else if (c <= 1114111)
 	{
 		ft_putchar((c >> 18) | 0xF0);
 		ft_putchar(((c >> 12) & 0x3F) | 0x80);
